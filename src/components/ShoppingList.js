4a4e99414a218a1d6f241ef9096903cc09c3d00f
Ralph Shui <rskc.2112@gmail.com>
2023-07-06 14:42:41 -0400

    completing info flow lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
+  function onCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
@@ -17,16 +18,11 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+        <Filter selectedCategory={selectedCategory} onCategoryChange={onCategoryChange}/>
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} {...item} />
         ))}
       </ul>
     </div>
